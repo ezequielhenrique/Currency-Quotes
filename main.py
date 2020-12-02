0dda2ad34ebb383566460d14c52618477f9ca6ad
e-henrry <ezequielhenrique806@gmail.com>
2020-12-02 15:08:55 -0300

    Update Interface

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ class MainWindow:
         self.master = master
         self.master.title('Currency Quotes')
         self.master.geometry('800x600')
-        self.master.configure(background='white')
+        self.master.configure(background='#F2F2F2')
+        self.master.resizable(False, False)
 
         options = ['USD', 'EUR', 'JPY']
 
@@ -28,8 +29,17 @@ class MainWindow:
         menu_comb.set('USD')
         menu_comb.pack(pady='10')
 
-        panel = PanelQuotes(self.master)
+        panel = PanelQuotes(self.master, bg_color='#F2F2F2')
         panel.pack()
+
+        style = ttk.Style()
+        style.theme_use('alt')
+        style.configure('TButton', background='#1F3447', foreground='white', borderwidth=1, focusthickness=3,
+                        focuscolor='none', font=('Roboto', 16), relief='sunken')
+        style.map('TButton', background=[('active', '#1F3447')])
+
+        button_calc = ttk.Button(self.master, text='Calculadora')
+        button_calc.pack(ipady=5, ipadx=60, pady=30)
 
 
 if __name__ == '__main__':
